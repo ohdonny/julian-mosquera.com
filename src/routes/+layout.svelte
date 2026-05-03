3d6064b675db5f463fb02eea7d554554991d8841
khalid <ga.lid2002.km@gmail.com>
2026-05-03 16:34:42 +0100

    Fix AuthCallback

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,22 @@
 	import '@fontsource/alegreya-sans';
 	import Header from '$lib/Header.svelte';
 	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
 
 	let { children } = $props();
 
 	onMount(async () => {
-	  const identity = await import('@netlify/identity')
-	  await identity.handleAuthCallback()
-	})
+	  const identity = await import('@netlify/identity');
+	  const result = await identity.handleAuthCallback();
+
+	  if (result?.type === 'invite' && result.token) {
+			const password = prompt('set admin password')
+			if (password){
+			  await identity.acceptInvite(result.token, password)
+			  goto('/admin')
+			}
+		}
+	});
 </script>
 
 <div class="flex min-h-screen">
